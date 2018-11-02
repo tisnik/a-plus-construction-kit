d4804b5c0d129ca10829d108572b369291141b05
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-11-02 11:41:16 +0100

    First two page with forms

--- src/a+construction_kit/html_renderer.clj
+++ src/a+construction_kit/html_renderer.clj
@@ -24,25 +24,51 @@
 
 
 
-(defn render-front-page
+(defn render-select-app-page
     "Render front page of this application."
-    []
+    [app-types]
     (page/xhtml
         (widgets/header "/" {:include-calendar? true})
         [:body
             [:div {:class "container"}
                 (widgets/navigation-bar "/")
-                [:h3 "Vstup do systému"]
-                (form/form-to {:name "inputForm"} [:get "/areals"]
-                    [:span "Platnost od:"] "&nbsp;"
+                [:h3 "Please choose application type"]
+                (form/form-to {:name "inputForm"} [:get "/select-language"]
                     [:br]
-                    [:br]
-                    "&nbsp;"
-                    [:a {:href "/help_valid_from_settings"} [:img {:src "icons/help.gif"}]]
-                    [:br]
-                    [:br]
-                    [:button {:type "submit" :class "btn btn-success" :style "width:10em"} "Seznam areálů"]
+                    (for [app-type app-types]
+                        [:div
+                            (widgets/submit-button (:label app-type) "app-type" (:name app-type))
+                            [:br]
+                            [:br]])
                 )
+                [:div {:style "height: 10ex"}]
+                (widgets/footer)
+            ] ; </div class="container">
+        ] ; </body>
+))
+
+(defn render-select-language-page
+    [app-type app-type-label app-parts app-languages]
+    (page/xhtml
+        (widgets/header "/" {:include-calendar? true})
+        [:body
+            [:div {:class "container"}
+                (widgets/navigation-bar "/")
+                [:h3 "Language selection for " app-type-label]
+                [:br]
+                (form/form-to {:name "inputForm"} [:get "/configure-modules"]
+                    [:input {:type "hidden" :name "app-type" :id "app-type" :value app-type}]
+                    (if app-parts
+                        (for [part app-parts]
+                            [:span
+                                [:h4 part]
+                                (for [language (get app-languages part)]
+                                    (widgets/radio-button (str part "-language") false language language (str "language_part_selected('" part "'," (count app-parts) ")")))
+                                [:br]])
+                        (for [language app-languages]
+                            (widgets/radio-button "primary-language" false language language "enable_next_button()")))
+                        [:br]
+                        (widgets/disabled-submit-button "Next" "next" "next"))
                 [:div {:style "height: 10ex"}]
                 (widgets/footer)
             ] ; </div class="container">
