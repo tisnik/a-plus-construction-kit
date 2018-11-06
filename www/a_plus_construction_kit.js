32a204b6f24a087b6d7397edbc39e17a4a3a89c7
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-11-06 09:27:20 +0100

    Use common style used by JS community

--- www/a_plus_construction_kit.js
+++ www/a_plus_construction_kit.js
@@ -1,29 +1,25 @@
-function enable_next_button()
-{
+function enableNextButton() {
     var button = document.getElementById("next");
     button.disabled = false;
 }
 
 var language_parts = {};
 
-function language_part_selected(part, app_parts)
-{
+function languagePartSelected(part, app_parts) {
     language_parts[part] = true;
     var cnt = Object.keys(language_parts).length;
     if (cnt >= app_parts) {
-        enable_next_button();
+        enableNextButton();
     }
 }
 
 var paper = null;
 
-function createPaper(width, height)
-{
+function createPaper(width, height) {
     paper = new Raphael(document.getElementById('canvas_container'), width, height);
 }
 
-function drawAppSchema(paper)
-{
+function drawAppSchema(paper) {
     var circle = paper.circle(100, 100, 80);
 }
 
