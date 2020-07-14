9b58bbc35b47d7a8a8a1c6c4ebd1b116408d81b0
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-07-14 09:13:26 +0200

    Basic unit tests for the model module

---
+++ test/a+construction_kit/model_test.clj
@@ -0,0 +1,31 @@
+;
+;  (C) Copyright 2017, 2020  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
+(ns a+construction-kit.config-test
+  (:require [clojure.test :refer :all]
+            [a+construction-kit.model :refer :all]
+            [clojure.pprint :as pprint]))
+
+;
+; Common functions used by tests.
+;
+
+(defn callable?
+    "Test if given function-name is bound to the real function."
+    [function-name]
+    (clojure.test/function? function-name))
+
+;
+; Tests for various functions
+;
+
+
