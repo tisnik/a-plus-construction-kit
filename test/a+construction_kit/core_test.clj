fbc1c0073df7ec6c2de2a1b3d1429dc83625bcd3
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-07-14 09:13:44 +0200

    Basic tests for core module

--- test/a+construction_kit/core_test.clj
+++ test/a+construction_kit/core_test.clj
@@ -1,7 +1,52 @@
+;
+;  (C) Copyright 2018, 2020  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
 (ns a+construction-kit.core-test
   (:require [clojure.test :refer :all]
             [a+construction-kit.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+;
+; Common functions used by tests.
+;
+
+(defn callable?
+    "Test if given function-name is bound to the real function."
+    [function-name]
+    (clojure.test/function? function-name))
+
+;
+; Tests for various functions
+;
+
+(deftest test-start-server-existence
+  "Check that the a+construction-kit.core/start-server definition exists."
+  (testing "if the a+construction-kit.core/start-server definition exists."
+    (is (callable? 'a+construction-kit.core/start-server))))
+
+
+(deftest test-show-help-existence
+  "Check that the a+construction-kit.core/show-help definition exists."
+  (testing "if the a+construction-kit.core/show-help definition exists."
+    (is (callable? 'a+construction-kit.core/show-help))))
+
+
+(deftest test-show-configuration-existence
+  "Check that the a+construction-kit.core/show-configuration definition exists."
+  (testing "if the a+construction-kit.core/show-configuration definition exists."
+    (is (callable? 'a+construction-kit.core/show-configuration))))
+
+
+(deftest test--main-existence
+  "Check that the a+construction-kit.core/-main definition exists."
+  (testing "if the a+construction-kit.core/-main definition exists."
+    (is (callable? 'a+construction-kit.core/-main))))
+
