05a8b80996e30ff5f2bfa43ef17c6c59d066755a
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-07-14 09:12:57 +0200

    Updated (c) year in project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 ;
-;  (C) Copyright 2018  Pavel Tisnovsky
+;  (C) Copyright 2018, 2020  Pavel Tisnovsky
 ;
 ;  All rights reserved. This program and the accompanying materials
 ;  are made available under the terms of the Eclipse Public License v1.0
