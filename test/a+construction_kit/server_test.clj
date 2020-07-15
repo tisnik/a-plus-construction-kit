6dc27d28386db45706cc944ed56c09cdd40cc39e
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-07-15 10:16:58 +0200

    Reformatted server_test.clj by zprint

--- test/a+construction_kit/server_test.clj
+++ test/a+construction_kit/server_test.clj
@@ -20,9 +20,9 @@
 ;
 
 (defn callable?
-    "Test if given function-name is bound to the real function."
-    [function-name]
-    (clojure.test/function? function-name))
+  "Test if given function-name is bound to the real function."
+  [function-name]
+  (clojure.test/function? function-name))
 
 ;
 ; Tests for various functions
@@ -31,54 +31,60 @@
 
 (deftest test-finish-processing-existence
   "Check that the a+construction-kit.server/finish-processing definition exists."
-  (testing "if the a+construction-kit.server/finish-processing definition exists."
+  (testing
+    "if the a+construction-kit.server/finish-processing definition exists."
     (is (callable? 'a+construction-kit.server/finish-processing))))
 
 
 (deftest test-process-select-app-page-existence
   "Check that the a+construction-kit.server/process-select-app-page definition exists."
-  (testing "if the a+construction-kit.server/process-select-app-page definition exists."
+  (testing
+    "if the a+construction-kit.server/process-select-app-page definition exists."
     (is (callable? 'a+construction-kit.server/process-select-app-page))))
 
 
 (deftest test-app-type-name->label-existence
   "Check that the a+construction-kit.server/app-type-name->label definition exists."
-  (testing "if the a+construction-kit.server/app-type-name->label definition exists."
+  (testing
+    "if the a+construction-kit.server/app-type-name->label definition exists."
     (is (callable? 'a+construction-kit.server/app-type-name->label))))
 
 
 (deftest test-read-languages-existence
   "Check that the a+construction-kit.server/read-languages definition exists."
   (testing "if the a+construction-kit.server/read-languages definition exists."
-    (is (callable? 'a+construction-kit.server/read-languages))))
+           (is (callable? 'a+construction-kit.server/read-languages))))
 
 
 (deftest test-process-select-language-page-existence
   "Check that the a+construction-kit.server/process-select-language-page definition exists."
-  (testing "if the a+construction-kit.server/process-select-language-page definition exists."
+  (testing
+    "if the a+construction-kit.server/process-select-language-page definition exists."
     (is (callable? 'a+construction-kit.server/process-select-language-page))))
 
 
 (deftest test-process-configure-modules-page-existence
   "Check that the a+construction-kit.server/process-configure-modules-page definition exists."
-  (testing "if the a+construction-kit.server/process-configure-modules-page definition exists."
+  (testing
+    "if the a+construction-kit.server/process-configure-modules-page definition exists."
     (is (callable? 'a+construction-kit.server/process-configure-modules-page))))
 
 
 (deftest test-uri->file-name-existence
   "Check that the a+construction-kit.server/uri->file-name definition exists."
   (testing "if the a+construction-kit.server/uri->file-name definition exists."
-    (is (callable? 'a+construction-kit.server/uri->file-name))))
+           (is (callable? 'a+construction-kit.server/uri->file-name))))
 
 
 (deftest test-gui-call-handler-existence
   "Check that the a+construction-kit.server/gui-call-handler definition exists."
-  (testing "if the a+construction-kit.server/gui-call-handler definition exists."
+  (testing
+    "if the a+construction-kit.server/gui-call-handler definition exists."
     (is (callable? 'a+construction-kit.server/gui-call-handler))))
 
 
 (deftest test-handler-existence
   "Check that the a+construction-kit.server/handler definition exists."
   (testing "if the a+construction-kit.server/handler definition exists."
-    (is (callable? 'a+construction-kit.server/handler))))
+           (is (callable? 'a+construction-kit.server/handler))))
 
