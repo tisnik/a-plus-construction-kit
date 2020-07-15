1960f299f886d1da9214a07f0dc2f914a5db022e
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-07-15 09:11:05 +0200

    Reformatted config.clj by zprint

--- src/a+construction_kit/config.clj
+++ src/a+construction_kit/config.clj
@@ -11,7 +11,7 @@
 ;
 
 (ns a+construction-kit.config
-    "Configuration loader and parser.
+  "Configuration loader and parser.
 
     Author: Pavel Tisnovsky")
 
@@ -21,35 +21,47 @@
 
 
 (defn full-prefix
-    "Construct full prefix for all REST API endpoints (including API version)."
-    [configuration]
-    (str (-> configuration :api :prefix) "/" (-> configuration :api :version)))
+  "Construct full prefix for all REST API endpoints (including API version)."
+  [configuration]
+  (str (-> configuration
+           :api
+           :prefix)
+       "/"
+       (-> configuration
+           :api
+           :version)))
 
 
 (defn update-configuration
-    "Update all selected items in the configuration structure.
+  "Update all selected items in the configuration structure.
      Other items are stored with string values."
-    [configuration]
-    (-> configuration
-        (update-in [:config        :verbose]          utils/parse-boolean)
-        (update-in [:config        :pretty-print]     utils/parse-boolean)))
+  [configuration]
+  (-> configuration
+      (update-in [:config :verbose] utils/parse-boolean)
+      (update-in [:config :pretty-print] utils/parse-boolean)))
 
 
 (defn load-configuration-from-ini
-    "Load configuration from the provided INI file and perform conversions
+  "Load configuration from the provided INI file and perform conversions
      on selected items from strings to numeric or Boolean values."
-    [ini-file-name]
-    (-> (config-loader/load-configuration-file ini-file-name)
-        update-configuration))
+  [ini-file-name]
+  (-> (config-loader/load-configuration-file ini-file-name)
+      update-configuration))
 
 
 (defn pretty-print?
-    "Read the pretty-print settings (it is used mainly for JSON output etc.)"
-    [request]
-    (-> request :configuration :config :pretty-print))
+  "Read the pretty-print settings (it is used mainly for JSON output etc.)"
+  [request]
+  (-> request
+      :configuration
+      :config
+      :pretty-print))
 
 
 (defn verbose?
-    "Read the verbose settings (it is used for all outputs that does not went into logs)"
-    [request]
-    (-> request :configuration :config :verbose))
+  "Read the verbose settings (it is used for all outputs that does not went into logs)"
+  [request]
+  (-> request
+      :configuration
+      :config
+      :verbose))
