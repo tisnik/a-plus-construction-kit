9b50ccdb0715bcf86b675100e720c653d358ce29
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-07-15 09:11:05 +0200

    Reformatted model.clj by zprint

--- src/a+construction_kit/model.clj
+++ src/a+construction_kit/model.clj
@@ -25,49 +25,49 @@
       :name  "cli-tool"}])
 
 (def app-parts
-    {"web-application" ["Back end" "Front end"]})
+  {"web-application" ["Back end" "Front end"]})
 
 (def app-languages
-    {"microservice"       ["Java" "Python" "JavaScript" "TypeScript" "Go"]
-     "web-application" {"Back end"
-                          ["Java" "Python" "JavaScript" "TypeScript" "Go"]
-                        "Front end"
-                          ["JavaScript" "TypeScript"]}
-     "openshift-cron-job" ["Java" "Python" "JavaScript" "TypeScript" "Go" "BASH"]
-     "desktop"            ["Java" "Python" "C" "C++" "Go"]
-     "cli-tool"           ["Java" "Python" "C" "C++" "Go" "BASH"]})
+  {"microservice"       ["Java" "Python" "JavaScript" "TypeScript" "Go"]
+   "web-application" {"Back end"
+                        ["Java" "Python" "JavaScript" "TypeScript" "Go"]
+                      "Front end"
+                        ["JavaScript" "TypeScript"]}
+   "openshift-cron-job" ["Java" "Python" "JavaScript" "TypeScript" "Go" "BASH"]
+   "desktop"            ["Java" "Python" "C" "C++" "Go"]
+   "cli-tool"           ["Java" "Python" "C" "C++" "Go" "BASH"]})
 
 (def configurations
-    {"microservice" ["Web service framework"
-                     "Data storage"
-                     "Message queuing service"
-                     "Other interfaces"
-                     "Libraries"]})
+  {"microservice" ["Web service framework"
+                   "Data storage"
+                   "Message queuing service"
+                   "Other interfaces"
+                   "Libraries"]})
 
 (def subgroups
-    {"Data storage"     ["Relational database"
-                         "Key-value database"
-                         "Document-oriented database"
-                         "Graph database"
-                         "Other NoSQL database"]
-     "Other interfaces" ["Information system"
-                         "CI"]
-     "Libraries"        ["Logging" "Networking" "Image manipulation" "Plotting"]
+  {"Data storage"     ["Relational database"
+                       "Key-value database"
+                       "Document-oriented database"
+                       "Graph database"
+                       "Other NoSQL database"]
+   "Other interfaces" ["Information system"
+                       "CI"]
+   "Libraries"        ["Logging" "Networking" "Image manipulation" "Plotting"]
 })
 
 ;
 (def config-values
-    {"Python" {"Web service framework"      ["Flask" "web.py" "Bottle"]
-               "Relational database"        ["DB/2" "Oracle RDBMS" "PostgreSQL" "MariaDB/MySQL" "SQLite" "ODBC"]
-               "Key-value database"         ["Dynamo", "Oracle NoSQL Database" "memcached" "Redis" "Berkeley DB"]
-               "Document-oriented database" ["Caché" "CouchDB" "MongoDB"]
-               "Graph database"             ["JanusGraph" "Neo4j" "Oracle Spatial and Graph"]
-               "Other NoSQL database"       ["BaseX" "eXist" "Sedna"]
-               "Message queuing service"    ["Microsoft Azure Service Bus" "Oracle Messaging Cloud Service" "Amazon Simple Queue Service" "IronMQ" "StormMQ" "AnypointMQ"]
-               "Information system"         ["SAP" "WordPress" "Joomla" "Drupal" "Google Maps" "Yahoo Maps"]
-               "CI"                         ["Jenkins" "Buildbot" "Travis CI"]
-               "Logging"                    ["logging" "logging2" "fastlog"]     
-               "Image manipulation"         ["Pillow"]
-               "Networking"                 ["asyncoro" "Gevent" "TwistedMatrix" "RPyC" "PyRO"]
-               "Plotting"                   ["gnuplot.py" "Matplotlib"]
-    }})                                                                          
+  {"Python" {"Web service framework"      ["Flask" "web.py" "Bottle"]
+             "Relational database"        ["DB/2" "Oracle RDBMS" "PostgreSQL" "MariaDB/MySQL" "SQLite" "ODBC"]
+             "Key-value database"         ["Dynamo", "Oracle NoSQL Database" "memcached" "Redis" "Berkeley DB"]
+             "Document-oriented database" ["Caché" "CouchDB" "MongoDB"]
+             "Graph database"             ["JanusGraph" "Neo4j" "Oracle Spatial and Graph"]
+             "Other NoSQL database"       ["BaseX" "eXist" "Sedna"]
+             "Message queuing service"    ["Microsoft Azure Service Bus" "Oracle Messaging Cloud Service" "Amazon Simple Queue Service" "IronMQ" "StormMQ" "AnypointMQ"]
+             "Information system"         ["SAP" "WordPress" "Joomla" "Drupal" "Google Maps" "Yahoo Maps"]
+             "CI"                         ["Jenkins" "Buildbot" "Travis CI"]
+             "Logging"                    ["logging" "logging2" "fastlog"]     
+             "Image manipulation"         ["Pillow"]
+             "Networking"                 ["asyncoro" "Gevent" "TwistedMatrix" "RPyC" "PyRO"]
+             "Plotting"                   ["gnuplot.py" "Matplotlib"]
+  }})                                                                          
