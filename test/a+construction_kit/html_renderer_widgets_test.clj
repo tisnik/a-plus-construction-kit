ff01d3adb8c074b41cf3ff466fc755489e3a490a
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-07-15 10:16:58 +0200

    Reformatted html_renderer_widgets_test.clj by zprint

--- test/a+construction_kit/html_renderer_widgets_test.clj
+++ test/a+construction_kit/html_renderer_widgets_test.clj
@@ -20,9 +20,9 @@
 ;
 
 (defn callable?
-    "Test if given function-name is bound to the real function."
-    [function-name]
-    (clojure.test/function? function-name))
+  "Test if given function-name is bound to the real function."
+  [function-name]
+  (clojure.test/function? function-name))
 
 ;
 ; Tests for various functions
@@ -31,72 +31,85 @@
 
 (deftest test-header-existence
   "Check that the a+construction-kit.html-renderer-widgets/header definition exists."
-  (testing "if the a+construction-kit.html-renderer-widgets/header definition exists."
+  (testing
+    "if the a+construction-kit.html-renderer-widgets/header definition exists."
     (is (callable? 'a+construction-kit.html-renderer-widgets/header))))
 
 
 (deftest test-navigation-bar-existence
   "Check that the a+construction-kit.html-renderer-widgets/navigation-bar definition exists."
-  (testing "if the a+construction-kit.html-renderer-widgets/navigation-bar definition exists."
+  (testing
+    "if the a+construction-kit.html-renderer-widgets/navigation-bar definition exists."
     (is (callable? 'a+construction-kit.html-renderer-widgets/navigation-bar))))
 
 
 (deftest test-footer-existence
   "Check that the a+construction-kit.html-renderer-widgets/footer definition exists."
-  (testing "if the a+construction-kit.html-renderer-widgets/footer definition exists."
+  (testing
+    "if the a+construction-kit.html-renderer-widgets/footer definition exists."
     (is (callable? 'a+construction-kit.html-renderer-widgets/footer))))
 
 
 (deftest test-back-button-existence
   "Check that the a+construction-kit.html-renderer-widgets/back-button definition exists."
-  (testing "if the a+construction-kit.html-renderer-widgets/back-button definition exists."
+  (testing
+    "if the a+construction-kit.html-renderer-widgets/back-button definition exists."
     (is (callable? 'a+construction-kit.html-renderer-widgets/back-button))))
 
 
 (deftest test-submit-button-existence
   "Check that the a+construction-kit.html-renderer-widgets/submit-button definition exists."
-  (testing "if the a+construction-kit.html-renderer-widgets/submit-button definition exists."
+  (testing
+    "if the a+construction-kit.html-renderer-widgets/submit-button definition exists."
     (is (callable? 'a+construction-kit.html-renderer-widgets/submit-button))))
 
 
 (deftest test-add-button-existence
   "Check that the a+construction-kit.html-renderer-widgets/add-button definition exists."
-  (testing "if the a+construction-kit.html-renderer-widgets/add-button definition exists."
+  (testing
+    "if the a+construction-kit.html-renderer-widgets/add-button definition exists."
     (is (callable? 'a+construction-kit.html-renderer-widgets/add-button))))
 
 
 (deftest test-remove-button-existence
   "Check that the a+construction-kit.html-renderer-widgets/remove-button definition exists."
-  (testing "if the a+construction-kit.html-renderer-widgets/remove-button definition exists."
+  (testing
+    "if the a+construction-kit.html-renderer-widgets/remove-button definition exists."
     (is (callable? 'a+construction-kit.html-renderer-widgets/remove-button))))
 
 
 (deftest test-disabled-submit-button-existence
   "Check that the a+construction-kit.html-renderer-widgets/disabled-submit-button definition exists."
-  (testing "if the a+construction-kit.html-renderer-widgets/disabled-submit-button definition exists."
-    (is (callable? 'a+construction-kit.html-renderer-widgets/disabled-submit-button))))
+  (testing
+    "if the a+construction-kit.html-renderer-widgets/disabled-submit-button definition exists."
+    (is (callable?
+          'a+construction-kit.html-renderer-widgets/disabled-submit-button))))
 
 
 (deftest test-radio-button-existence
   "Check that the a+construction-kit.html-renderer-widgets/radio-button definition exists."
-  (testing "if the a+construction-kit.html-renderer-widgets/radio-button definition exists."
+  (testing
+    "if the a+construction-kit.html-renderer-widgets/radio-button definition exists."
     (is (callable? 'a+construction-kit.html-renderer-widgets/radio-button))))
 
 
 (deftest test-help-button-existence
   "Check that the a+construction-kit.html-renderer-widgets/help-button definition exists."
-  (testing "if the a+construction-kit.html-renderer-widgets/help-button definition exists."
+  (testing
+    "if the a+construction-kit.html-renderer-widgets/help-button definition exists."
     (is (callable? 'a+construction-kit.html-renderer-widgets/help-button))))
 
 
 (deftest test-canvas-existence
   "Check that the a+construction-kit.html-renderer-widgets/canvas definition exists."
-  (testing "if the a+construction-kit.html-renderer-widgets/canvas definition exists."
+  (testing
+    "if the a+construction-kit.html-renderer-widgets/canvas definition exists."
     (is (callable? 'a+construction-kit.html-renderer-widgets/canvas))))
 
 
 (deftest test-drop-down-existence
   "Check that the a+construction-kit.html-renderer-widgets/drop-down definition exists."
-  (testing "if the a+construction-kit.html-renderer-widgets/drop-down definition exists."
+  (testing
+    "if the a+construction-kit.html-renderer-widgets/drop-down definition exists."
     (is (callable? 'a+construction-kit.html-renderer-widgets/drop-down))))
 
