931bb46dbd02b0bdc04b8d65d1aaf0d10ef91d53
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-07-15 10:16:58 +0200

    Reformatted config_test.clj by zprint

--- test/a+construction_kit/config_test.clj
+++ test/a+construction_kit/config_test.clj
@@ -20,9 +20,9 @@
 ;
 
 (defn callable?
-    "Test if given function-name is bound to the real function."
-    [function-name]
-    (clojure.test/function? function-name))
+  "Test if given function-name is bound to the real function."
+  [function-name]
+  (clojure.test/function? function-name))
 
 ;
 ; Tests for various functions
@@ -31,29 +31,31 @@
 (deftest test-full-prefix-existence
   "Check that the a+construction-kit.config/full-prefix definition exists."
   (testing "if the a+construction-kit.config/full-prefix definition exists."
-    (is (callable? 'a+construction-kit.config/full-prefix))))
+           (is (callable? 'a+construction-kit.config/full-prefix))))
 
 
 (deftest test-update-configuration-existence
   "Check that the a+construction-kit.config/update-configuration definition exists."
-  (testing "if the a+construction-kit.config/update-configuration definition exists."
+  (testing
+    "if the a+construction-kit.config/update-configuration definition exists."
     (is (callable? 'a+construction-kit.config/update-configuration))))
 
 
 (deftest test-load-configuration-from-ini-existence
   "Check that the a+construction-kit.config/load-configuration-from-ini definition exists."
-  (testing "if the a+construction-kit.config/load-configuration-from-ini definition exists."
+  (testing
+    "if the a+construction-kit.config/load-configuration-from-ini definition exists."
     (is (callable? 'a+construction-kit.config/load-configuration-from-ini))))
 
 
 (deftest test-pretty-print?-existence
   "Check that the a+construction-kit.config/pretty-print? definition exists."
   (testing "if the a+construction-kit.config/pretty-print? definition exists."
-    (is (callable? 'a+construction-kit.config/pretty-print?))))
+           (is (callable? 'a+construction-kit.config/pretty-print?))))
 
 
 (deftest test-verbose?-existence
   "Check that the a+construction-kit.config/verbose? definition exists."
   (testing "if the a+construction-kit.config/verbose? definition exists."
-    (is (callable? 'a+construction-kit.config/verbose?))))
+           (is (callable? 'a+construction-kit.config/verbose?))))
 
