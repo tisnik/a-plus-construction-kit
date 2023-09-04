30b7f52a04776b3bf5a75a4f1215e5f2ffbf5fb6
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-09-04 08:46:49 +0200

    Updated (c) year in tests

--- test/a+construction_kit/html_renderer_test.clj
+++ test/a+construction_kit/html_renderer_test.clj
@@ -1,5 +1,5 @@
 ;
-;  (C) Copyright 2017, 2020  Pavel Tisnovsky
+;  (C) Copyright 2017, 2020, 2023  Pavel Tisnovsky
 ;
 ;  All rights reserved. This program and the accompanying materials
 ;  are made available under the terms of the Eclipse Public License v1.0
