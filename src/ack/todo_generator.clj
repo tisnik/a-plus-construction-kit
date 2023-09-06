ba93d7c2886024da5eb2925a2c192c09e7597b69
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-09-06 12:13:17 +0200

    Model splitted by language

--- src/ack/todo_generator.clj
+++ src/ack/todo_generator.clj
@@ -30,8 +30,8 @@
              :download      nil}
     :Python {:title         "Project stub will be prepared"
              :description   "Project structure will be generated based on Python programming language"
-             :list-of-files ["setup.py" "requirements.in" "project.py" "license.txt"]
-             :command       "python project.py"
+             :list-of-files ["setup.py" "requirements.in" "requirements.txt" "project.py" "license.txt"]
+             :command       "python project.py\npip install -m requirements.txt"
              :download      nil}
     })
 
@@ -57,59 +57,106 @@
 
 
 (def add-web-service-framework
-  {
-   :Gorilla-Toolkit {:title         "Gorilla Toolkit dependency"
-                     :description   "Gorilla Toolkit package will be added into the project"
-                     :list-of-files ["gorilla_interface.go" "go.mod" "go.sum"]
-                     :command       "go get github.com/gorilla\ngo mod tidy"
-                     :download      nil}
-   })
+  {:Go
+    {
+     :Gorilla-Toolkit {:title         "Gorilla Toolkit dependency"
+                       :description   "Gorilla Toolkit package will be added into the project"
+                       :list-of-files ["gorilla_interface.go" "go.mod" "go.sum"]
+                       :command       "go get github.com/gorilla\ngo mod tidy"
+                       :download      nil}
+     }
+  :Python
+   {
+    :Flask            {:title         "Flask dependency"
+                       :description   "Flask framework will be added into the project"
+                       :list-of-files ["flask.py" "requirements.in" "requirements.txt"]
+                       :command       "pip install -i requirements.txt"
+                       :download      nil}
+   }})
 
 
 (def add-logging-library
-  {
-   :log             {:title         "Standard log library dependency"
-                     :description   "Standard log library does not need any further step"
-                     :list-of-files nil
-                     :command       nil
-                     :download      nil}
-   :zerolog         {:title         "Zerolog library dependency"
-                     :description   "Zerolog library will be added into the project"
-                     :list-of-files ["go.mod" "go.sum"]
-                     :command       "go get github.com/zerolog\ngo mod tidy"
-                     :download      nil}
-   })
+  {:Go
+    {
+     :log             {:title         "Standard log library dependency"
+                       :description   "Standard log library does not need any further step"
+                       :list-of-files nil
+                       :command       nil
+                       :download      nil}
+     :zerolog         {:title         "Zerolog library dependency"
+                       :description   "Zerolog library will be added into the project"
+                       :list-of-files ["go.mod" "go.sum"]
+                       :command       "go get github.com/zerolog\ngo mod tidy"
+                       :download      nil}
+     }
+    :Python
+     {
+     :logging         {:title         "Logging library dependency"
+                       :description   "Logging library will be added into the project as dependency"
+                       :list-of-files ["logging.py" "requirements.in" "requirements.txt"]
+                       :command       "pip install -i requirements.txt"
+                       :download      nil}
+      }})
 
 
 (def add-streaming-platform
-  {
-   :Apache-Kafka  {:title         "Interface to Apache Kafka"
-                   :description   "Interface to Apache Kafka will be added as a dependency. Configuration will be changed as well."
-                   :list-of-files ["kafka.go" "go.mod" "go.sum" "config.toml" "server.properties" "zookeeper.properties"]
-                   :command       "go get github.com/sarama\ngo mod tidy\ncd kafka\nbin/kafka-server-start.sh config/server.properties\nbin/zookeeper-server-start.sh config/zookeeper.properties"
-                   :download      nil}
-   :JetStream     {:title         "Interface to JetStream"
-                   :description   "Interface to JetStream will be added as a dependency. Configuration will be changed as well."
-                   :list-of-files ["nats.cfg"]
-                   :command       "nats redis.cfg"
-                   :download      nil}
-   :Redis-streams {:title         "Interface to Redis Streams"
-                   :description   "Interface to Redis Streams will be added as a dependency. Configuration will be changed as well."
-                   :list-of-files ["redis.go" "go.mod" "go.sum" "config.toml" "redis.cfg"]
-                   :command       "go get github.com/redis\ngo mod tidy\nredis-server redis.cfg"
-                   :download      nil}
-   })
+  {:Go
+    {
+     :Apache-Kafka  {:title         "Interface to Apache Kafka"
+                     :description   "Interface to Apache Kafka will be added as a dependency. Configuration will be changed as well."
+                     :list-of-files ["kafka.go" "go.mod" "go.sum" "config.toml" "server.properties" "zookeeper.properties"]
+                     :command       "go get github.com/sarama\ngo mod tidy\ncd kafka\nbin/kafka-server-start.sh config/server.properties\nbin/zookeeper-server-start.sh config/zookeeper.properties"
+                     :download      nil}
+     :JetStream     {:title         "Interface to JetStream"
+                     :description   "Interface to JetStream will be added as a dependency. Configuration will be changed as well."
+                     :list-of-files ["nats.cfg"]
+                     :command       "nats redis.cfg"
+                     :download      nil}
+     :Redis-streams {:title         "Interface to Redis Streams"
+                     :description   "Interface to Redis Streams will be added as a dependency. Configuration will be changed as well."
+                     :list-of-files ["redis.go" "go.mod" "go.sum" "config.toml" "redis.cfg"]
+                     :command       "go get github.com/redis\ngo mod tidy\nredis-server redis.cfg"
+                     :download      nil}
+     }
+   :Python
+    {
+     :Apache-Kafka  {:title         "Interface to Apache Kafka"
+                     :description   "Interface to Apache Kafka will be added as a dependency. Configuration will be changed as well."
+                     :list-of-files ["kafka.py" "setup.py" "requierements.txt" "server.properties" "zookeeper.properties"]
+                     :command       "pip install -i requirements.txt\ncd kafka\nbin/kafka-server-start.sh config/server.properties\nbin/zookeeper-server-start.sh config/zookeeper.properties"
+                     :download      nil}
+     :JetStream     {:title         "Interface to JetStream"
+                     :description   "Interface to JetStream will be added as a dependency. Configuration will be changed as well."
+                     :list-of-files ["nats.py" "setup.py" "requierements.txt" "nats.cfg"]
+                     :command       "pip install -i requirements.txt\nnats redis.cfg"
+                     :download      nil}
+     :Redis-streams {:title         "Interface to Redis Streams"
+                     :description   "Interface to Redis Streams will be added as a dependency. Configuration will be changed as well."
+                     :list-of-files ["redis.py" "setup.py" "requierements.txt"]
+                     :command       "pip install -i requirements.txt\nredis-server redis.cfg"
+                     :download      nil}
+     }
+    })
 
+
+(defn not-nil
+  [x]
+  (or x ""))
 
 (defn get-item
   [input item]
   (if item
-    (-> input item (string/replace " " "-") keyword)))
+    (-> input item not-nil (string/replace " " "-") keyword)))
 
 
 (defn todo-item
   [input item dict]
   ((get-item input item) dict))
+
+
+(defn todo-item-language
+  [input item dict]
+  (todo-item input item ((keyword (:primary-language input)) dict)))
 
 
 (defn generate-model
@@ -123,11 +170,11 @@
 (defn generate-todo-list
   [model]
   [
-   (todo-item model :primary-language      project-stub)
-   (todo-item model :web_service_framework add-web-service-framework)
-   (todo-item model :streaming_platform    add-streaming-platform)
-   (todo-item model :logging               add-logging-library)
-   (todo-item model :deployment-type       deployment-type)
+   (todo-item model          :primary-language      project-stub)
+   (todo-item-language model :web_service_framework add-web-service-framework)
+   (todo-item-language model :streaming_platform    add-streaming-platform)
+   (todo-item-language model :logging               add-logging-library)
+   (todo-item model          :deployment-type       deployment-type)
   ])
 
 
